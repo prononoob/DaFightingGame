2f1f1a3ce17a9298e3f6714ccf9d50795563674a
prononoob <lollel60606@gmail.com>
2021-03-29 15:52:45 +0200

    added basic .py code, not expected to work yet

---
+++ DaGame.py
@@ -0,0 +1,23 @@
+import random
+
+
+pHealth=[]
+nHealth=[]
+
+pMana=[]
+nMana=[]
+
+whoseTurn=[]
+
+#dmg = random.randint
+#heal = random.randint
+
+
+def main():
+    while True:
+        playerTurn()
+        checkScore()
+        npcTurn()
+        checkscore()
+
+
