5d2bce2ad9d4d3281c0d18d7404a44f20802be9e
prononoob <lollel60606@gmail.com>
2021-03-29 16:27:25 +0200

    added basic code that works

--- DaGame.py
+++ DaGame.py
@@ -1,23 +1,54 @@
 import random
 
 
-pHealth=[]
-nHealth=[]
+pHealth=[1,1,1,1,1,1,1,1,1,1]
+nHealth=[1,1,1,1,1,1,1,1,1,1]
 
-pMana=[]
-nMana=[]
+pMana=[1,1,1,1,1]
+nMana=[1,1,1,1,1]
 
-whoseTurn=[]
+whoseTurn=[0]
 
 #dmg = random.randint
 #heal = random.randint
 
+
+def playerTurn():
+    z=input('Attack or use spell? (a/s): ')
+    if z.lower()=='a':
+        attack()
+    elif z.lower()=='s':
+        spell()
+    else:
+        print('Skipping turn... ')
+
+def npcTurn():
+    print('npcTurn... (not working yet)')
+
+def checkScore():
+    if len(pHealth)==0:
+        print('NPC won! ')
+        exit()
+    elif len(nHealth)==0:
+        print('Player won! ')
+        exit()
+    else:
+        print('Player\'s Health: ',len(pHealth))
+        print('NPC\'s Health: ',len(nHealth))
+        print('Next turn! ')
+    
+def attack():
+   nHealth.pop(0)
+   print('Attacked enemy! ')
+
+def spell():
+    nHealth.pop(0)
 
 def main():
     while True:
         playerTurn()
         checkScore()
         npcTurn()
-        checkscore()
+        checkScore()
 
-
+main()
